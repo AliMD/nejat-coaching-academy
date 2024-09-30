da2626a42417b8d999977c19b51d7289d6130999
Mohammad Honarvar <honarvar.info@gmail.com>
2024-09-30 13:41:45 +0330

    fxi(api): use `cryptoFactory` to generate an `id` for an agent

--- packages/api/src/route/agent.ts
+++ packages/api/src/route/agent.ts
@@ -1,4 +1,5 @@
 import {config, logger} from '../lib/config.js';
+import {cryptoFactory} from '../lib/crypto.js';
 import {nanotronApiServer} from '../lib/server.js';
 import {alwatrStore} from '../lib/store.js';
 import {parseRequestBody} from '../pre-handler/parse-request-body.js';
@@ -12,9 +13,11 @@ nanotronApiServer.defineRoute<{requestBody: AgentFormData}>({
   async handler() {
     logger.logMethodArgs?.('defineRoute(`/agent/save`)', {body: this.sharedMeta.requestBody});
 
-    // add new agent to the agent's collection
     const agentsCollection = await alwatrStore.openCollection<AgentFormData>(config.stores.agentsCollection);
-    const agentId = agentsCollection.appendItem(this.sharedMeta.requestBody);
+
+    // add new agent to the agent's collection
+    const agentId = cryptoFactory.generateUserId();
+    agentsCollection.addItem(agentId, this.sharedMeta.requestBody);
     agentsCollection.save();
 
     this.serverResponse.replyJson({
