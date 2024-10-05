712e1189acc591d20694521eae968679abf6a2cf
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-05 14:08:52 +0330

    feat(app): apply custom class to base element in connectedCallback

--- packages/app/src/ts/component/base-element.ts
+++ packages/app/src/ts/component/base-element.ts
@@ -3,9 +3,16 @@ import {LitElement} from 'lit';
 export class BaseElement extends LitElement {
   customClass = 'block';
 
-  constructor() {
-    super();
+  override connectedCallback(): void {
+    super.connectedCallback();
+    this.setCustomClass_();
+  }
+
+  private customClassApplied_ = false;
+  private setCustomClass_() {
+    if (this.customClassApplied_) return;
     this.className = `${this.customClass} ${this.className}`;
+    this.customClassApplied_ = true;
   }
 
   protected override createRenderRoot(): HTMLElement | DocumentFragment {
