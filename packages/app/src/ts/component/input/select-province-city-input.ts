a6c52715edf2dc7a8e124e34379a54ef6e2f486a
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-05 15:30:11 +0330

    refactor(app): update select input to use optional chaining

--- packages/app/src/ts/component/input/select-province-city-input.ts
+++ packages/app/src/ts/component/input/select-province-city-input.ts
@@ -3,7 +3,7 @@ import {html, type PropertyValues} from 'lit';
 import {customElement, property, query} from 'lit/decorators.js';
 
 import {BaseElement} from '../base-element.js';
-import './select-input.js';
+import {type SelectInputComponent} from './select-input.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -17,36 +17,30 @@ export class SelectProvinceCityInputComponent extends BaseElement {
   @property({type: Array, state: true}) cities?: CityItem[];
 
   @query('select-input[name="province"]', true)
-  private provinceSelectElement__?: HTMLInputElement;
+  private provinceSelectElement__?: SelectInputComponent;
 
   @query('select-input[name="city"]', true)
-  private citySelectElement__?: HTMLInputElement;
+  private citySelectElement__?: SelectInputComponent;
 
-  private value_: { cityId: string; provinceId: string };
   get value() {
-    this.value_ = {
-      provinceId: this.provinceSelectElement__!.value,
-      cityId: this.citySelectElement__!.value,
+    return {
+      provinceId: this.provinceSelectElement__?.value,
+      cityId: this.citySelectElement__?.value,
     };
-
-    return this.value_;
   }
 
   override customClass = 'flex gap-3';
 
   constructor() {
     super();
-
-    this.value_ = {
-      cityId: '',
-      provinceId: ''
-    };
     this.inputDir = 'rtl';
-    this.cities = [];
+    this.cities = provinceOptions[1].cities;
   }
 
   protected onProvinceChange_() {
-    this.cities = provinceOptions[this.provinceSelectElement__!.value].cities;
+    const provinceId = this.provinceSelectElement__?.value;
+    if (provinceId === undefined) return;
+    this.cities = provinceOptions[provinceId].cities;
   }
 
   override render() {
@@ -63,13 +57,8 @@ export class SelectProvinceCityInputComponent extends BaseElement {
         class="basis-2/5"
         name="province"
         label="شهر"
-        .options=${this.cities!.map(item => ({value: item.id, label: item.label}))}
+        .options=${this.cities?.map(item => ({value: item.id, label: item.label}))}
       ></select-input>
     `;
-  }
-
-  protected override firstUpdated(_changedProperties: PropertyValues): void {
-    super.firstUpdated(_changedProperties);
-    this.onProvinceChange_();
   }
 }
