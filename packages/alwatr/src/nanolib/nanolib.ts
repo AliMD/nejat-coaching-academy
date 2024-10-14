cc01c36c9803bde449c9e184351003d1005816aa
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-14 21:50:31 +0330

    fix(nanolib/resolveUrl):Add leading slash if the first part has it

--- packages/alwatr/src/nanolib/nanolib.ts
+++ packages/alwatr/src/nanolib/nanolib.ts
@@ -1,12 +1,12 @@
 export * from '@alwatr/nanolib';
 
 const trimSlashes = /* #__PURE__ */ /(?:^\/+|\/+$)/g;
-const trimEndSlashes = /* #__PURE__ */ /\/+$/g;
 const multipleSlashes = /* #__PURE__ */ /(?<!:)\/{2,}/g;
 
 export function resolveUrl(...parts: string[]): string {
-  return parts
-    .map((part, index) => part.replace(index === 0 ? trimEndSlashes : trimSlashes, '')) // Remove leading and trailing slashes
+  const prefix = parts[0].indexOf('/') === 0 ? '/' : ''; // Add leading slash if the first part has it
+  return prefix + parts
+    .map((part) => part.replace(trimSlashes, '')) // Remove leading and trailing slashes
     .filter((part) => part) // Remove empty parts
     .join('/')
     .replace(multipleSlashes, '/'); // Replace multiple slashes with a single slash, except for protocol
