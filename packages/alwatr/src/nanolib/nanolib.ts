c8fd6499aea4cc2b0b77ca6c0da1e1007195a7e9
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-14 12:29:14 +0330

    feat(nanolib): resolveUrl resolution function

--- packages/alwatr/src/nanolib/nanolib.ts
+++ packages/alwatr/src/nanolib/nanolib.ts
@@ -1,1 +1,17 @@
 export * from '@alwatr/nanolib';
+
+const trimSlashes = /* #__PURE__ */ /(^\/+|\/+$)/g;
+const multipleSlashes = /* #__PURE__ */ /(?<!:)\/{2,}/g;
+
+export function resolveUrl(...parts: string[]): string {
+  return parts
+    .map(part => part.replace(trimSlashes, '')) // Remove leading and trailing slashes
+    .filter(part => part) // Remove empty parts
+    .join('/')
+    .replace(multipleSlashes, '/'); // Replace multiple slashes with a single slash, except for protocol
+}
+
+// Examples
+// console.log(resolveUrl('/', 'ali', 'v1')); // '/ali/v1'
+// console.log(resolveUrl('/', '/ali/', '/v1')); // '/ali/v1'
+// console.log(resolveUrl('https://domain.com', 'ali', 'v1')); // 'https://domain.com/ali/v1'
