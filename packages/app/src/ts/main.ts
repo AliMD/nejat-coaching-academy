dce460c737910760e31d8561687d17a267aea07d
MohammadHonarvar <honarvar.info@gmail.com>
2024-10-19 13:58:14 +0330

    fix(app): import path

--- packages/app/src/ts/main.ts
+++ packages/app/src/ts/main.ts
@@ -1,9 +1,9 @@
 import './component/display-user-data/main.js';
 import './component/invited-user-form/main.js';
-import './component/user-form.js';
+import './component/register-form.js';
 import {logger} from './lib/config.js';
 
 logger.logFileModule?.('main');
 
-logger.banner('Swiss Plus Support');
+logger.banner('Nejat Coaching Academy');
 
