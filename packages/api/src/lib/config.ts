a7ca2ece26f6ca17cfbc44dea2b2a41317434066
Mohammad Honarvar <honarvar.info@gmail.com>
2024-10-20 13:59:24 +0330

    refactor(api): move `nitrobase` stats to `common` package

--- packages/api/src/lib/config.ts
+++ packages/api/src/lib/config.ts
@@ -1,7 +1,7 @@
 import {createLogger, packageTracer, type FetchOptions} from 'alwatr/nanolib';
-import {Region, StoreFileType, type AlwatrNitrobaseConfig, type StoreFileStat} from 'alwatr/nitrobase';
 
 import type {CryptoFactoryConfig, NanotronApiServerConfig} from 'alwatr/nanotron';
+import type {AlwatrNitrobaseConfig} from 'alwatr/nitrobase';
 
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
 
@@ -49,30 +49,6 @@ export const config = {
     config: {
       rootPath: env.dbPath!,
     } as AlwatrNitrobaseConfig,
-
-    userInfoDocument: {
-      name: 'user-info',
-      region: Region.PerOwner,
-      type: StoreFileType.Document,
-    } as StoreFileStat,
-
-    phoneDocument: {
-      name: 'phone',
-      region: Region.PerOwner,
-      type: StoreFileType.Document,
-    } as StoreFileStat,
-
-    authDocument: {
-      name: 'auth',
-      region: Region.PerOwner,
-      type: StoreFileType.Document,
-    } as StoreFileStat,
-
-    invitationCodeDocument: {
-      name: 'invitation-code',
-      region: Region.PerOwner,
-      type: StoreFileType.Document,
-    } as StoreFileStat,
   } as const,
 
   fetchOptions: {
--- packages/api/src/lib/intialize-nitrobase.ts
+++ packages/api/src/lib/intialize-nitrobase.ts
@@ -1,20 +1,21 @@
-import {config} from './config.js';
+import {nitrobaseStats} from 'common';
+
 import {alwatrNitrobase} from './nitrobase.js';
 
 export function initializeStore() {
-  if (alwatrNitrobase.hasStore(config.nitrobase.authDocument) === false) {
-    alwatrNitrobase.newCollection(config.nitrobase.authDocument);
+  if (alwatrNitrobase.hasStore(nitrobaseStats.authDocument) === false) {
+    alwatrNitrobase.newCollection(nitrobaseStats.authDocument);
   }
 
-  if (alwatrNitrobase.hasStore(config.nitrobase.userInfoDocument) === false) {
-    alwatrNitrobase.newCollection(config.nitrobase.userInfoDocument);
+  if (alwatrNitrobase.hasStore(nitrobaseStats.userInfoDocument) === false) {
+    alwatrNitrobase.newCollection(nitrobaseStats.userInfoDocument);
   }
 
-  if (alwatrNitrobase.hasStore(config.nitrobase.phoneDocument) === false) {
-    alwatrNitrobase.newCollection(config.nitrobase.phoneDocument);
+  if (alwatrNitrobase.hasStore(nitrobaseStats.phoneDocument) === false) {
+    alwatrNitrobase.newCollection(nitrobaseStats.phoneDocument);
   }
 
-  if (alwatrNitrobase.hasStore(config.nitrobase.invitationCodeDocument) === false) {
-    alwatrNitrobase.newCollection(config.nitrobase.invitationCodeDocument);
+  if (alwatrNitrobase.hasStore(nitrobaseStats.invitationCodeDocument) === false) {
+    alwatrNitrobase.newCollection(nitrobaseStats.invitationCodeDocument);
   }
 }
